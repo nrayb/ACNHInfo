bf0d5640e1ee0f415e597ce4bbc2e50aa09525bc
Noel Ray Barron <noelraybarron@hotmail.com>
2020-04-20 22:42:58 -0700

    Same deal, but with bugs instead of fish

---
+++ utils/BugsTableParser.ts
@@ -0,0 +1,210 @@
+// All the info is extracted from this site:
+// https://animalcrossing.fandom.com/wiki/Bugs_(New_Horizons)
+
+
+const northernHemisphereData = `Common butterfly	NH-Icon-commonbutterfly	160	Flying	4 AM - 7 PM	✓	✓	✓	✓	✓	✓	-	-	✓	✓	✓	✓
+Yellow butterfly	NH-Icon-yellowbutterfly	160	Flying	4 AM - 7 PM	-	-	✓	✓	✓	✓	-	-	✓	✓	-	-
+Tiger butterfly	NH-Icon-tigerbutterfly	240	Flying	4 AM - 7 PM	-	-	✓	✓	✓	✓	✓	✓	✓	-	-	-
+Peacock butterfly	NH-Icon-peacockbutterfly	2500	Flying by Hybrid Flowers	4 AM - 7 PM	-	-	✓	✓	✓	✓	-	-	-	-	-	-
+Common bluebottle	NH-Icon-commonbluebottle	300	Flying	4 AM - 7 PM	-	-	-	✓	✓	✓	✓	✓	-	-	-	-
+Paper kite butterfly	NH-Icon-paperkitebutterfly	1000	Flying	8 AM - 7 PM	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓
+Great purple emperor	NH-Icon-greatpurpleemperor	3000	Flying	4 AM - 7 PM	-	-	-	-	✓	✓	✓	✓	-	-	-	-
+Monarch butterfly	NH-Icon-monarchbutterfly	140	Flying	4 AM - 5 PM	-	-	-	-	-	-	-	-	✓	✓	✓	-
+Emperor butterfly	NH-Icon-emperorbutterfly	4000	Flying	5 PM - 8 AM	✓	✓	✓	-	-	✓	✓	✓	✓	-	-	✓
+Agrias butterfly	NH-Icon-agriasbutterfly	3000	Flying	8 AM - 5 PM	-	-	-	✓	✓	✓	✓	✓	✓	-	-	-
+Rajah Brooke's birdwing	NH-Icon-rajahbrookesbirdwing	2500	Flying	8 AM - 5 PM	✓	✓	-	✓	✓	✓	✓	✓	✓	-	-	✓
+Queen Alexandra's birdwing	NH-Icon-queenalexandrasbirdwing	4000	Flying	8 AM - 4 PM	-	-	-	-	✓	✓	✓	✓	✓	-	-	-
+Moth	NH-Icon-moth	130	Flying by Light	7 PM - 4 AM	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓
+Atlas moth	NH-Icon-atlasmoth	3000	On Trees	7 PM - 4 AM	-	-	-	✓	✓	✓	✓	✓	✓	-	-	-
+Madagascan sunset moth	NH-Icon-madagascansunsetmoth	2500	Flying	8 AM - 4 PM	-	-	-	✓	✓	✓	✓	✓	✓	-	-	-
+Long locust	NH-Icon-longlocust	200	On the Ground	8 AM - 7 PM	-	-	-	✓	✓	✓	✓	✓	✓	✓	✓	-
+Migratory locust	NH-Icon-migratorylocust	600	On the Ground	8 AM - 7 PM	-	-	-	-	-	-	-	✓	✓	✓	✓	-
+Rice grasshopper	NH-Icon-ricegrasshopper	160	On the Ground	8 AM - 7 PM	-	-	-	-	-	-	-	✓	✓	✓	✓	-
+Grasshopper	NH-Icon-grasshopper	160	On the Ground	8 AM - 5 PM	-	-	-	-	-	-	✓	✓	✓	-	-	-
+Cricket	NH-Icon-cricket	130	On the Ground	5 PM - 8 AM	-	-	-	-	-	-	-	-	✓	✓	✓	-
+Bell cricket	NH-Icon-bellcricket	430	On the Ground	5 PM - 8 AM	-	-	-	-	-	-	-	-	✓	✓	-	-
+Mantis	NH-Icon-mantis	430	On Flowers	8 AM - 5 PM	-	-	✓	✓	✓	✓	✓	✓	✓	✓	✓	-
+Orchid mantis	NH-Icon-orchidmantis	2400	On Flowers (White)	8 AM - 5 PM	-	-	✓	✓	✓	✓	✓	✓	✓	✓	✓	-
+Honeybee	NH-Icon-honeybee	200	Flying	8 AM - 5 PM	-	-	✓	✓	✓	✓	✓	-	-	-	-	-
+Wasp	NH-Icon-wasp	2500	Shaking Trees	All day	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓
+Brown cicada	NH-Icon-browncicada	250	On Trees	8 AM - 5 PM	-	-	-	-	-	-	✓	✓	-	-	-	-
+Robust cicada	NH-Icon-robustcicada	300	On Trees	8 AM - 5 PM	-	-	-	-	-	-	✓	✓	-	-	-	-
+Giant cicada	NH-Icon-giantcicada	500	On Trees	8 AM - 5 PM	-	-	-	-	-	-	✓	✓	-	-	-	-
+Walker cicada	NH-Icon-walkercicada	400	On Trees	8 AM - 5 PM	-	-	-	-	-	-	-	✓	✓	-	-	-
+Evening cicada	NH-Icon-eveningcicada	550	On Trees	4 AM - 8 AM & 4 PM - 7 PM	-	-	-	-	-	-	✓	✓	-	-	-	-
+Cicada shell	NH-Icon-cicadashell	10	On Trees	All day	-	-	-	-	-	-	✓	✓	-	-	-	-
+Red dragonfly	NH-Icon-reddragonfly	180	Flying	8 AM - 7 PM	-	-	-	-	-	-	-	-	✓	✓	-	-
+Darner dragonfly	NH-Icon-darnerdragonfly	230	Flying	8 AM - 5 PM	-	-	-	✓	✓	✓	✓	✓	✓	✓	-	-
+Banded dragonfly	NH-Icon-bandeddragonfly	4500	Flying	8 AM - 5 PM	-	-	-	-	✓	✓	✓	✓	✓	✓	-	-
+Damselfly	NH-Icon-damselfly	500	Flying	All day	✓	✓	-	-	-	-	-	-	-	-	✓	✓
+Firefly	NH-Icon-firefly	300	Flying	7 PM - 4 AM	-	-	-	-	-	✓	-	-	-	-	-	-
+Mole cricket	NH-Icon-molecricket	500	Underground	All day	✓	✓	✓	✓	✓	-	-	-	-	-	✓	✓
+Pondskater	NH-Icon-pondskater	130	On Ponds and Rivers	8 AM - 7 PM	-	-	-	-	✓	✓	✓	✓	✓	-	-	-
+Diving beetle	NH-Icon-divingbeetle	800	On Ponds and Rivers	8 AM - 7 PM	-	-	-	-	✓	✓	✓	✓	✓	-	-	-
+Giant water bug	NH-Icon-giantwaterbug	2000	On Ponds and Rivers	7 PM - 8 AM	-	-	-	✓	✓	✓	✓	✓	✓	-	-	-
+Stinkbug	NH-Icon-stinkbug	120	On Flowers	All day	-	-	✓	✓	✓	✓	✓	✓	✓	✓	-	-
+Man-faced stink bug	NH-Icon-manfacedstinkbug	1000	On Flowers	7 PM - 8 AM	-	-	✓	✓	✓	✓	✓	✓	✓	✓	-	-
+Ladybug	NH-Icon-ladybug	200	On Flowers	8 AM - 5 PM	-	-	✓	✓	✓	✓	-	-	-	✓	-	-
+Tiger beetle	NH-Icon-tigerbeetle	1500	On the Ground	All day	-	✓	✓	✓	✓	✓	✓	✓	✓	✓	-	-
+Jewel beetle	NH-Icon-jewelbeetle	2400	On Tree Stumps	All day	-	-	-	✓	✓	✓	✓	✓	-	-	-	-
+Violin beetle	NH-Icon-violinbeetle	450	On Tree Stumps	All day	-	-	-	-	✓	✓	-	-	✓	✓	✓	-
+Citrus long-horned beetle	NH-Icon-citruslonghornedbeetle	350	On Tree Stumps	All day	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓
+Rosalia batesi beetle	NH-Icon-rosaliabatesibeetle	3000	On Tree Stumps	All day	-	-	-	-	✓	✓	✓	✓	✓	-	-	-
+Blue weevil beetle	NH-Icon-blueweevilbeetle	800	On Trees (Coconut?)	All day	-	-	-	-	-	-	✓	✓	-	-	-	-
+Dung beetle	NH-Icon-dungbeetle	3000	On the Ground (rolling snowballs)	All day	✓	✓	-	-	-	-	-	-	-	-	-	✓
+Earth-boring dung beetle	NH-Icon-earthboringdungbeetle	300	On the Ground	All day	-	-	-	-	-	-	✓	✓	✓	-	-	-
+Scarab beetle	NH-Icon-scarabbeetle	10000	On Trees	11 PM - 8 AM	-	-	-	-	-	-	✓	✓	-	-	-	-
+Drone beetle	NH-Icon-dronebeetle	200	On Trees	All day	-	-	-	-	-	✓	✓	✓	-	-	-	-
+Goliath beetle	NH-Icon-goliathbeetle	8000	On Trees (Coconut)	5 PM - 8 AM	-	-	-	-	-	✓	✓	✓	✓	-	-	-
+Saw stag	NH-Icon-sawstag	2000	On Trees	All day	-	-	-	-	-	-	✓	✓	-	-	-	-
+Miyama stag	NH-Icon-miyamastag	1000	On Trees	All day	-	-	-	-	-	-	✓	✓	-	-	-	-
+Giant stag	NH-Icon-giantstag	10000	On Trees	11 PM - 8 AM	-	-	-	-	-	-	✓	✓	-	-	-	-
+Rainbow stag	NH-Icon-rainbowstag	6000	On Trees	7 PM - 8 AM	-	-	-	-	-	✓	✓	✓	✓	-	-	-
+Cyclommatus stag	NH-Icon-cyclommatusstag	8000	On Trees (Coconut)	5 PM - 8 AM	-	-	-	-	-	-	✓	✓	-	-	-	-
+Golden stag	NH-Icon-goldenstag	12000	On Trees (Coconut)	5 PM - 8 AM	-	-	-	-	-	-	✓	✓	-	-	-	-
+Giraffe stag	NH-Icon-giraffestag	12000	On Trees (Coconut?)	5 PM - 8 AM	-	-	-	-	-	-	✓	✓	-	-	-	-
+Horned dynastid	NH-Icon-horneddynastid	1350	On Trees	5 PM - 8 AM	-	-	-	-	-	-	✓	✓	-	-	-	-
+Horned atlas	NH-Icon-hornedatlas	8000	On Trees (Coconut)	5 PM - 8 AM	-	-	-	-	-	-	✓	✓	-	-	-	-
+Horned elephant	NH-Icon-hornedelephant	8000	On Trees (Coconut)	5 PM - 8 AM	-	-	-	-	-	-	✓	✓	-	-	-	-
+Horned hercules	NH-Icon-hornedhercules	12000	On Trees (Coconut)	5 PM - 8 AM	-	-	-	-	-	-	✓	✓	-	-	-	-
+Walking stick	NH-Icon-walkingstick	600	On Trees	4 AM - 8 AM & 5 PM - 7 PM	-	-	-	-	-	-	✓	✓	✓	✓	✓	-
+Walking leaf	NH-Icon-walkingleaf	600	Under Trees Disguised as Leafs	All day	-	-	-	-	-	-	✓	✓	✓	-	-	-
+Bagworm	NH-Icon-bagworm	600	Shaking Trees	All day	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓
+Ant	NH-Icon-ant	80	On rotten food	All day	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓
+Hermit crab	NH-Icon-hermitcrab	1000	Beach disguised as Shells	7 PM - 8 AM	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓
+Wharf roach	NH-Icon-wharfroach	200	On Beach Rocks	All day	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓
+Fly	NH-Icon-fly	60	On Trash Items	All day	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓
+Mosquito	NH-Icon-mosquito	130	Flying	5 PM - 4 AM	-	-	-	-	-	✓	✓	✓	✓	-	-	-
+Flea	NH-Icon-flea	70	Villager's Heads	All day	-	-	-	✓	✓	✓	✓	✓	✓	✓	✓	-
+Snail	NH-Icon-snail	250	On Rocks (Rain)	All day	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓
+Pill bug	NH-Icon-pillbug	250	Hitting Rocks	11 PM - 4 PM	✓	✓	✓	✓	✓	✓	-	-	✓	✓	✓	✓
+Centipede	NH-Icon-centipede	300	Hitting Rocks	4 PM - 11 PM	✓	✓	✓	✓	✓	✓	-	-	✓	✓	✓	✓
+Spider	NH-Icon-spider	600	Shaking Trees	7 PM - 8 AM	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓
+Tarantula	NH-Icon-tarantula	8000	On the Ground	7 PM - 4 AM	✓	✓	✓	✓	-	-	-	-	-	-	✓	✓
+Scorpion	NH-Icon-scorpion	8000	On the Ground	7 PM - 4 AM	-	-	-	-	✓	✓	✓	✓	✓	✓	-	-`;
+
+const southernHemisphereData = `Common butterfly	NH-Icon-commonbutterfly	160	Flying	4am - 7pm	-	-	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓
+Yellow butterfly	NH-Icon-yellowbutterfly	160	Flying	4am - 7pm	-	-	✓	✓	-	-	-	-	✓	✓	✓	✓
+Tiger butterfly	NH-Icon-tigerbutterfly	240	Flying	4am - 7pm	✓	✓	✓	-	-	-	-	-	✓	✓	✓	✓
+Peacock butterfly	NH-Icon-peacockbutterfly	2500	Flying by hybrid flowers	4am - 7pm	-	-	-	-	-	-	-	-	✓	✓	✓	✓
+Common bluebottle	NH-Icon-commonbluebottle	300	Flying	4am - 7pm	✓	✓	-	-	-	-	-	-	-	✓	✓	✓
+Paper kite butterfly	NH-Icon-paperkitebutterfly	1000	Flying	8am - 7pm	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓
+Great purple emperor	NH-Icon-greatpurpleemperor	3000	Flying	4am - 7pm	✓	✓	-	-	-	-	-	-	-	-	✓	✓
+Monarch butterfly	NH-Icon-monarchbutterfly	140	Flying	4am - 5pm	-	-	✓	✓	✓	-	-	-	-	-	-	-
+Emperor butterfly	NH-Icon-emperorbutterfly	4000	Flying	5pm - 8am	✓	✓	✓	-	-	✓	✓	✓	✓	-	-	✓
+Agrias butterfly	NH-Icon-agriasbutterfly	3000	Flying	8am - 5pm	✓	✓	✓	-	-	-	-	-	-	✓	✓	✓
+Rajah Brooke's birdwing	NH-Icon-rajahbrookesbirdwing	2500	Flying	8am - 5pm	✓	✓	✓	-	-	✓	✓	✓	-	✓	✓	✓
+Queen Alexandra's birdwing	NH-Icon-queenalexandrasbirdwing	4000	Flying	8am - 4pm	✓	✓	✓	-	-	-	-	-	-	-	✓	✓
+Moth	NH-Icon-moth	130	Flying near light sources	7pm - 4am	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓
+Atlas moth	NH-Icon-atlasmoth	3000	On trees	7pm - 4am	✓	✓	✓	-	-	-	-	-	-	✓	✓	✓
+Madagascan sunset moth	NH-Icon-madagascansunsetmoth	2500	Flying	8am - 4pm	✓	✓	✓	-	-	-	-	-	-	✓	✓	✓
+Long locust	NH-Icon-longlocust	200	On the ground	8am - 7pm	✓	✓	✓	✓	✓	-	-	-	-	✓	✓	✓
+Migratory locust	NH-Icon-migratorylocust	600	On the ground	8am - 7pm	-	✓	✓	✓	✓	-	-	-	-	-	-	-
+Rice grasshopper	NH-Icon-ricegrasshopper	160	On the ground	8am - 7pm	-	✓	✓	✓	✓	-	-	-	-	-	-	-
+Grasshopper	NH-Icon-grasshopper	160	On the ground	8am - 5pm	✓	✓	✓	-	-	-	-	-	-	-	-	-
+Cricket	NH-Icon-cricket	130	On the ground	5pm - 8am	-	-	✓	✓	✓	-	-	-	-	-	-	-
+Bell cricket	NH-Icon-bellcricket	430	On the ground	5pm - 8am	-	-	✓	✓	-	-	-	-	-	-	-	-
+Mantis	NH-Icon-mantis	430	On flowers	8am - 5pm	✓	✓	✓	✓	✓	-	-	-	✓	✓	✓	✓
+Orchid mantis	NH-Icon-orchidmantis	2400	On white flowers	8am - 5pm	✓	✓	✓	✓	✓	-	-	-	✓	✓	✓	✓
+Honeybee	NH-Icon-honeybee	200	Flying	8am - 5pm	✓	-	-	-	-	-	-	-	✓	✓	✓	✓
+Wasp	NH-Icon-wasp	2500	Shaking trees	All day	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓
+Brown cicada	NH-Icon-browncicada	250	On trees	8am - 5pm	✓	✓	-	-	-	-	-	-	-	-	-	-
+Robust cicada	NH-Icon-robustcicada	300	On trees	8am - 5pm	✓	✓	-	-	-	-	-	-	-	-	-	-
+Giant cicada	NH-Icon-giantcicada	500	On trees	8am - 5pm	✓	✓	-	-	-	-	-	-	-	-	-	-
+Walker cicada	NH-Icon-walkercicada	400	On trees	8am - 5pm	-	✓	✓	-	-	-	-	-	-	-	-	-
+Evening cicada	NH-Icon-eveningcicada	550	On trees	4am - 8am & 4pm - 7pm	✓	✓	-	-	-	-	-	-	-	-	-	-
+Cicada shell	NH-Icon-cicadashell	10	On trees	All day	✓	✓	-	-	-	-	-	-	-	-	-	-
+Red dragonfly	NH-Icon-reddragonfly	180	Flying	8am - 7pm	-	-	✓	✓	-	-	-	-	-	-	-	-
+Darner dragonfly	NH-Icon-darnerdragonfly	230	Flying	8am - 5pm	✓	✓	✓	✓	-	-	-	-	-	✓	✓	✓
+Banded dragonfly	NH-Icon-bandeddragonfly	4500	Flying	8am - 5pm	✓	✓	✓	✓	-	-	-	-	-	-	✓	✓
+Damselfly	NH-Icon-damselfly	500	Flying	All day	-	-	-	-	✓	✓	✓	✓	-	-	-	-
+Firefly	NH-Icon-firefly	300	Flying	7pm - 4am	-	-	-	-	-	-	-	-	-	-	-	✓
+Mole cricket	NH-Icon-molecricket	500	Underground	All day	-	-	-	-	✓	✓	✓	✓	✓	✓	✓	-
+Pondskater	NH-Icon-pondskater	130	On fresh water	8am - 7pm	✓	✓	✓	-	-	-	-	-	-	-	✓	✓
+Diving beetle	NH-Icon-divingbeetle	800	On fresh water	8am - 7pm	✓	✓	✓	-	-	-	-	-	-	-	✓	✓
+Giant water bug	NH-Icon-giantwaterbug	2000	On fresh water	7pm - 8am	✓	✓	✓	-	-	-	-	-	-	✓	✓	✓
+Stinkbug	NH-Icon-stinkbug	120	On flowers	All day	✓	✓	✓	✓	-	-	-	-	✓	✓	✓	✓
+Man-faced stink bug	NH-Icon-manfacedstinkbug	1000	On flowers	7pm - 8am	✓	✓	✓	✓	-	-	-	-	✓	✓	✓	✓
+Ladybug	NH-Icon-ladybug	200	On flowers	8am - 5pm	-	-	-	✓	-	-	-	-	✓	✓	✓	✓
+Tiger beetle	NH-Icon-tigerbeetle	1500	On the ground	All day	✓	✓	✓	✓	-	-	-	✓	✓	✓	✓	✓
+Jewel beetle	NH-Icon-jewelbeetle	2400	On tree stumps	All day	✓	✓	-	-	-	-	-	-	-	✓	✓	✓
+Violin beetle	NH-Icon-violinbeetle	450	On tree stumps	All day	-	-	✓	✓	✓	-	-	-	-	-	✓	✓
+Citrus long-horned beetle	NH-Icon-citruslonghornedbeetle	350	On tree stumps	All day	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓
+Rosalia batesi beetle	NH-Icon-rosaliabatesibeetle	3000	On tree stumps	All day	✓	✓	✓	-	-	-	-	-	-	-	✓	✓
+Blue weevil beetle	NH-Icon-blueweevilbeetle	800	On palm trees	All day	✓	✓	-	-	-	-	-	-	-	-	-	-
+Dung beetle	NH-Icon-dungbeetle	3000	Rolling snowballs along the ground	All day	-	-	-	-	-	✓	✓	✓	-	-	-	-
+Earth-boring dung beetle	NH-Icon-earthboringdungbeetle	300	On the ground	All day	✓	✓	✓	-	-	-	-	-	-	-	-	-
+Scarab beetle	NH-Icon-scarabbeetle	10000	On trees	11pm - 8am	✓	✓	-	-	-	-	-	-	-	-	-	-
+Drone beetle	NH-Icon-dronebeetle	200	On trees	All day	✓	✓	-	-	-	-	-	-	-	-	-	✓
+Goliath beetle	NH-Icon-goliathbeetle	8000	On palm trees	5pm - 8am	✓	✓	✓	-	-	-	-	-	-	-	-	✓
+Saw stag	NH-Icon-sawstag	2000	On trees	All day	✓	✓	-	-	-	-	-	-	-	-	-	-
+Miyama stag	NH-Icon-miyamastag	1000	On trees	All day	✓	✓	-	-	-	-	-	-	-	-	-	-
+Giant stag	NH-Icon-giantstag	10000	On trees	11pm - 8am	✓	✓	-	-	-	-	-	-	-	-	-	-
+Rainbow stag	NH-Icon-rainbowstag	6000	On trees	7pm - 8am	✓	✓	✓	-	-	-	-	-	-	-	-	✓
+Cyclommatus stag	NH-Icon-cyclommatusstag	8000	On palm trees	5pm - 8am	✓	✓	-	-	-	-	-	-	-	-	-	-
+Golden stag	NH-Icon-goldenstag	12000	On palm trees	5pm - 8am	✓	✓	-	-	-	-	-	-	-	-	-	-
+Giraffe stag	NH-Icon-giraffestag	12000	On palm trees	5pm - 8am	✓	✓	-	-	-	-	-	-	-	-	-	-
+Horned dynastid	NH-Icon-horneddynastid	1350	On palm trees	5pm - 8am	✓	✓	-	-	-	-	-	-	-	-	-	-
+Horned atlas	NH-Icon-hornedatlas	8000	On palm trees	5pm - 8am	✓	✓	-	-	-	-	-	-	-	-	-	-
+Horned elephant	NH-Icon-hornedelephant	8000	On palm trees	5pm - 8am	✓	✓	-	-	-	-	-	-	-	-	-	-
+Horned hercules	NH-Icon-hornedhercules	12000	On palm trees	5pm - 8am	✓	✓	-	-	-	-	-	-	-	-	-	-
+Walking stick	NH-Icon-walkingstick	600	On trees	4am - 8am & 5pm - 7pm	✓	✓	✓	✓	✓	-	-	-	-	-	-	-
+Walking leaf	NH-Icon-walkingleaf	600	Disguised as leaves	All day	✓	✓	✓	-	-	-	-	-	-	-	-	-
+Bagworm	NH-Icon-bagworm	600	Shaking trees	All day	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓
+Ant	NH-Icon-ant	80	On rotten food	All day	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓
+Hermit crab	NH-Icon-hermitcrab	1000	Disguised as shells	7pm - 8am	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓
+Wharf roach	NH-Icon-wharfroach	200	On rocks at the beach	All day	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓
+Fly	NH-Icon-fly	60	On trash	All day	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓
+Mosquito	NH-Icon-mosquito	130	Flying	5pm - 4am	✓	✓	✓	-	-	-	-	-	-	-	-	✓
+Flea	NH-Icon-flea	70	On villagers	All day	✓	✓	✓	✓	✓	-	-	-	-	✓	✓	✓
+Snail	NH-Icon-snail	250	On rocks during rain	All day	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓
+Pill bug	NH-Icon-pillbug	250	Hitting rocks	11pm - 4pm	-	-	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓
+Centipede	NH-Icon-centipede	300	Hitting rocks	4pm - 11pm	-	-	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓
+Spider	NH-Icon-spider	480	Shaking trees	7pm - 8am	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓
+Tarantula	NH-Icon-tarantula	8000	On the ground	7pm - 4am	-	-	-	-	✓	✓	✓	✓	✓	✓	-	-
+Scorpion	NH-Icon-scorpion	8000	On the ground	7pm - 4am	✓	✓	✓	✓	-	-	-	-	-	-	✓	✓`;
+
+const parsedData = northernHemisphereData.split(`\n`).map((rawRow: string) => {
+    const [ name, _imgLocation, price, location, spawnTime, jan, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec ] = rawRow.split(`\t`);
+    return {
+        name,
+        price,
+        location,
+        spawnTime,
+        nMonths: {
+            jan: jan === "✓",
+            feb: jan === "✓",
+            mar: mar === "✓",
+            apr: apr === "✓",
+            may: may === "✓",
+            jun: jun === "✓",
+            jul: jul === "✓",
+            aug: aug === "✓",
+            sep: sep === "✓",
+            oct: oct === "✓",
+            nov: nov === "✓",
+            dec: dec === "✓",
+        },
+    };
+});
+
+southernHemisphereData.split(`\n`).forEach((rawRow: string) => {
+    const [ name, _imgLocation, price, location, spawnTime, jan, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec ] = rawRow.split(`\t`);
+    const fish = parsedData.find(row => row.name === name) as any;
+    fish && (fish.sMonths = {
+        jan: jan === "✓",
+        feb: jan === "✓",
+        mar: mar === "✓",
+        apr: apr === "✓",
+        may: may === "✓",
+        jun: jun === "✓",
+        jul: jul === "✓",
+        aug: aug === "✓",
+        sep: sep === "✓",
+        oct: oct === "✓",
+        nov: nov === "✓",
+        dec: dec === "✓",
+    });
+});
+
+console.log(JSON.stringify(parsedData));
