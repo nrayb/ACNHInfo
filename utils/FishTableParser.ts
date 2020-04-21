2a0b277fd9784b29fb0c8559140ef60d743719c3
Noel Ray Barron <noelraybarron@hotmail.com>
2020-04-20 22:38:49 -0700

    Create Simple parser for fish data.
    Data was extracted (simply copy-paste) from the fandom site.
    
    The short script just formats it to my 1st-phase datasructure.
    
    It's really ugly right now, I know that, I will likely modify this soon.
    
    Also, one thing to consider would be to move this data to an
    S3 bucket. bundling the data to the app wouldn't be ideal as any
    updates to the core data would require an entire update to the app.
    
    I think it's okay for now, but again, consider doing the work to
    move it future Noel

---
+++ utils/FishTableParser.ts
@@ -0,0 +1,211 @@
+// All the info is extracted from this site:
+// https://animalcrossing.fandom.com/wiki/Fish_(New_Horizons)
+
+
+const northernHemisphereData = `Bitterling	NH-Icon-bitterling	900	River	1	All day	✓	✓	✓	-	-	-	-	-	-	-	✓	✓
+Pale chub	NH-Icon-palechub	200	River	1	9 AM - 4 PM	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓
+Crucian carp	NH-Icon-cruciancarp	160	River	2	All day	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓
+Dace	NH-Icon-dace	240	River	3	4 PM - 9 AM	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓
+Carp	NH-Icon-carp	300	Pond	4	All day	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓
+Koi	NH-Icon-koi	4000	Pond	4	4 PM - 9 AM	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓
+Goldfish	NH-Icon-goldfish	1300	Pond	1	All day	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓
+Pop-eyed goldfish	NH-Icon-popeyedgoldfish	1300	Pond	1	9 AM - 4 PM	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓
+Ranchu goldfish	NH-Icon-ranchugoldfish	4500	Pond	2	9 AM - 4 PM	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓
+Killifish	NH-Icon-killifish	300	Pond	1	All day	-	-	-	✓	✓	✓	✓	✓	-	-	-	-
+Crawfish	NH-Icon-crawfish	200	Pond	2	All day	-	-	-	✓	✓	✓	✓	✓	✓	-	-	-
+Soft-shelled turtle	NH-Icon-softshelledturtle	3750	River	4	4 PM - 9 AM	-	-	-	-	-	-	-	✓	✓	-	-	-
+Snapping Turtle	NH-Icon-snappingturtle	5000	River	4	9 PM - 4 AM	-	-	-	✓	✓	✓	✓	✓	✓	✓	-	-
+Tadpole	NH-Icon-tadpole	100	Pond	1	All day	-	-	✓	✓	✓	✓	✓	-	-	-	-	-
+Frog	NH-Icon-frog	120	Pond	2	All day	-	-	-	-	✓	✓	✓	✓	-	-	-	-
+Freshwater goby	NH-Icon-freshwatergoby	400	River	2	4 PM - 9 AM	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓
+Loach	NH-Icon-loach	400	River	2	All day	-	-	✓	✓	✓	-	-	-	-	-	-	-
+Catfish	NH-Icon-catfish	800	Pond	4	4 PM - 9 AM	-	-	-	-	✓	✓	✓	✓	✓	✓	-	-
+Giant snakehead	NH-Icon-giantsnakehead	5500	Pond	4	9 AM - 4 PM	-	-	-	-	-	✓	✓	✓	-	-	-	-
+Bluegill	NH-Icon-bluegill	180	River	2	9 AM - 4 PM	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓
+Yellow perch	NH-Icon-yellowperch	300	River	3	All day	✓	✓	✓	-	-	-	-	-	-	✓	✓	✓
+Black bass	NH-Icon-blackbass	400	River	4	All day	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓
+Tilapia	NH-Icon-tilapia	800	River	3	All day	-	-	-	-	-	✓	✓	✓	✓	✓	-	-
+Pike	NH-Icon-pike	1800	River	5	All day	-	-	-	-	-	-	-	-	✓	✓	✓	✓
+Pond smelt	NH-Icon-pondsmelt	500	River	2	All day	✓	✓	-	-	-	-	-	-	-	-	-	✓
+Sweetfish	NH-Icon-sweetfish	900	River	3	All day	-	-	-	-	-	-	✓	✓	✓	-	-	-
+Cherry salmon	NH-Icon-cherrysalmon	800	River (Clifftop)	3	4 PM - 9 AM	-	-	✓	✓	✓	✓	-	-	✓	✓	✓	-
+Char	NH-Icon-char	3800	River (Clifftop) Pond	3	4 PM - 9 AM	-	-	✓	✓	✓	✓	-	-	✓	✓	✓	-
+Golden trout	NH-Icon-goldentrout	15000	River (Clifftop)	3	4 PM - 9 AM	-	-	✓	✓	✓	-	-	-	✓	✓	✓	-
+Stringfish	NH-Icon-stringfish	15000	River (Clifftop)	5	4 PM - 9 AM	✓	✓	✓	-	-	-	-	-	-	-	-	✓
+Salmon	NH-Icon-salmon	700	River (Mouth)	4	All day	-	-	-	-	-	-	-	-	✓	-	-	-
+King salmon	NH-Icon-kingsalmon	1800	River (Mouth)	6	All day	-	-	-	-	-	-	-	-	✓	-	-	-
+Mitten crab	NH-Icon-mittencrab	2000	River	2	4 PM - 9 AM	-	-	-	-	-	-	-	-	✓	✓	✓	-
+Guppy	NH-Icon-guppy	1300	River	1	9 AM - 4 PM	-	-	-	✓	✓	✓	✓	✓	✓	✓	✓	-
+Nibble fish	NH-Icon-nibblefish	1500	River	1	9 AM - 4 PM	-	-	-	-	✓	✓	✓	✓	✓	-	-	-
+Angelfish	NH-Icon-angelfish	3000	River	2	4 PM - 9 AM	-	-	-	-	✓	✓	✓	✓	✓	✓	-	-
+Betta	NH-Icon-betta	2500	River	2	9 AM - 4 PM	-	-	-	-	✓	✓	✓	✓	✓	✓	-	-
+Neon tetra	NH-Icon-neontetra	500	River	1	9 AM - 4 PM	-	-	-	✓	✓	✓	✓	✓	✓	✓	✓	-
+Rainbowfish	NH-Icon-rainbowfish	800	River	1	9 AM - 4 PM	-	-	-	-	✓	✓	✓	✓	✓	✓	-	-
+Piranha	NH-Icon-piranha	2500	River	2	9 AM - 4 PM & 9 PM - 4 AM	-	-	-	-	-	✓	✓	✓	✓	-	-	-
+Arowana	NH-Icon-arowana	10000	River	4	4 PM - 9 AM	-	-	-	-	-	✓	✓	✓	✓	-	-	-
+Dorado	NH-Icon-dorado	15000	River	5	4 AM - 9 PM	-	-	-	-	-	✓	✓	✓	✓	-	-	-
+Gar	NH-Icon-gar	6000	Pond	5	4 PM - 9 AM	-	-	-	-	-	✓	✓	✓	✓	-	-	-
+Arapaima	NH-Icon-arapaima	10000	River	6	4 PM - 9 AM	-	-	-	-	-	✓	✓	✓	✓	-	-	-
+Saddled bichir	NH-Icon-saddledbichir	4000	River	4	9 PM - 4 AM	-	-	-	-	-	✓	✓	✓	✓	-	-	-
+Sturgeon	NH-Icon-sturgeon	10000	River (Mouth)	6	All day	✓	✓	✓	-	-	-	-	-	✓	✓	✓	✓
+Sea butterfly	NH-Icon-seabutterfly	1000	Sea	1	All day	✓	✓	✓	-	-	-	-	-	-	-	-	✓
+Sea horse	NH-Icon-seahorse	1100	Sea	1	All day	-	-	-	✓	✓	✓	✓	✓	✓	✓	✓	-
+Clown fish	NH-Icon-clownfish	650	Sea	1	All day	-	-	-	✓	✓	✓	✓	✓	✓	-	-	-
+Surgeonfish	NH-Icon-surgeonfish	1000	Sea	2	All day	-	-	-	✓	✓	✓	✓	✓	✓	-	-	-
+Butterfly fish	NH-Icon-butterflyfish	1000	Sea	2	All day	-	-	-	✓	✓	✓	✓	✓	✓	-	-	-
+Napoleonfish	NH-Icon-napoleonfish	10000	Sea	6	4 AM - 9 PM	-	-	-	-	-	-	✓	✓	-	-	-	-
+Zebra turkeyfish	NH-Icon-zebraturkeyfish	500	Sea	3	All day	-	-	-	✓	✓	✓	✓	✓	✓	✓	✓	-
+Blowfish	NH-Icon-blowfish	5000	Sea	3	9 PM - 4 AM	✓	✓	-	-	-	-	-	-	-	-	✓	✓
+Puffer fish	NH-Icon-pufferfish	250	Sea	3	All day	-	-	-	-	-	-	✓	✓	✓	-	-	-
+Anchovy	NH-Icon-anchovy	200	Sea	2	4 AM - 9 PM	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓
+Horse mackerel	NH-Icon-horsemackerel	150	Sea	2	All day	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓
+Barred knifejaw	NH-Icon-barredknifejaw	5000	Sea	3	All day	-	-	✓	✓	✓	✓	✓	✓	✓	✓	✓	-
+Sea bass	NH-Icon-seabass	400	Sea	5	All day	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓
+Red snapper	NH-Icon-redsnapper	3000	Sea	4	All day	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓
+Dab	NH-Icon-dab	300	Sea	3	All day	✓	✓	✓	✓	-	-	-	-	-	✓	✓	✓
+Olive flounder	NH-Icon-oliveflounder	800	Sea	5	All day	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓
+Squid	NH-Icon-squid	500	Sea	3	All day	✓	✓	✓	✓	✓	✓	✓	✓	-	-	-	✓
+Moray eel	NH-Icon-morayeel	2000	Sea	Narrow	All day	-	-	-	-	-	-	-	✓	✓	✓	-	-
+Ribbon eel	NH-Icon-ribboneel	600	Sea	Narrow	All day	-	-	-	-	-	✓	✓	✓	✓	✓	-	-
+Tuna	NH-Icon-tuna	7000	Pier	6	All day	✓	✓	✓	✓	-	-	-	-	-	-	✓	✓
+Blue marlin	NH-Icon-bluemarlin	10000	Pier	6	All day	✓	✓	✓	✓	-	-	✓	✓	✓	-	✓	✓
+Giant trevally	NH-Icon-gianttrevally	4500	Pier	5	All day	-	-	-	-	✓	✓	✓	✓	✓	✓	-	-
+Mahi-mahi	NH-Icon-mahimahi	6000	Pier	5	All day	-	-	-	-	✓	✓	✓	✓	✓	✓	-	-
+Ocean sunfish	NH-Icon-oceansunfish	4000	Sea	6 (Fin)	4 AM - 9 PM	-	-	-	-	-	-	✓	✓	✓	-	-	-
+Ray	NH-Icon-ray	3000	Sea	5	4 AM - 9 PM	-	-	-	-	-	-	-	✓	✓	✓	✓	-
+Saw shark	NH-Icon-sawshark	12000	Sea	6 (Fin)	4 PM - 9 AM	-	-	-	-	-	✓	✓	✓	✓	-	-	-
+Hammerhead shark	NH-Icon-hammerheadshark	8000	Sea	6 (Fin)	4 PM - 9 AM	-	-	-	-	-	✓	✓	✓	✓	-	-	-
+Great white shark	NH-Icon-greatwhiteshark	15000	Sea	6 (Fin)	4 PM - 9 AM	-	-	-	-	-	✓	✓	✓	✓	-	-	-
+Whale shark	NH-Icon-whaleshark	13000	Sea	6 (Fin)	All day	-	-	-	-	-	✓	✓	✓	✓	-	-	-
+Suckerfish	NH-Icon-suckerfish	1500	Sea	4 (Fin)	All day	-	-	-	-	-	✓	✓	✓	✓	-	-	-
+Football fish	NH-Icon-footballfish	2500	Sea	4	4 PM - 9 AM	✓	✓	✓	-	-	-	-	-	-	-	✓	✓
+Oarfish	NH-Icon-oarfish	9000	Sea	6	All day	✓	✓	✓	✓	✓	-	-	-	-	-	-	✓
+Barreleye	NH-Icon-barreleye	15000	Sea	2	9 PM - 4 AM	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓
+Coelacanth	NH-Icon-coelacanth	15000	Sea (while raining)	6	All day	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓`;
+
+const southernHemisphereData = `Bitterling	NH-Icon-bitterling	900	River	1	All day	-	-	-	-	✓	✓	✓	✓	✓	-	-	-
+Pale chub	NH-Icon-palechub	200	River	1	9am - 4pm	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓
+Crucian carp	NH-Icon-cruciancarp	160	River	2	All day	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓
+Dace	NH-Icon-dace	240	River	3	4pm - 9am	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓
+Carp	NH-Icon-carp	300	Pond	4	All day	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓
+Koi	NH-Icon-koi	4000	Pond	4	4pm - 9am	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓
+Goldfish	NH-Icon-goldfish	1300	Pond	1	All day	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓
+Pop-eyed goldfish	NH-Icon-popeyedgoldfish	1300	Pond	1	9am - 4pm	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓
+Ranchu goldfish	NH-Icon-ranchugoldfish	4500	Pond	2	9am - 4pm	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓
+Killifish	NH-Icon-killifish	300	Pond	1	All day	✓	✓	-	-	-	-	-	-	-	✓	✓	✓
+Crawfish	NH-Icon-crawfish	200	Pond	2	All day	✓	✓	✓	-	-	-	-	-	-	✓	✓	✓
+Soft-shelled turtle	NH-Icon-softshelledturtle	3750	River	4	4pm - 9am	-	✓	✓	-	-	-	-	-	-	-	-	-
+Snapping turtle	NH-Icon-snappingturtle	5000	River	4	9pm - 4am	✓	✓	✓	✓	-	-	-	-	-	✓	✓	✓
+Tadpole	NH-Icon-tadpole	100	Pond	1	All day	✓	-	-	-	-	-	-	-	✓	✓	✓	✓
+Frog	NH-Icon-frog	120	Pond	2	All day	✓	✓	-	-	-	-	-	-	-	-	✓	✓
+Freshwater goby	NH-Icon-freshwatergoby	400	River	2	4pm - 9am	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓
+Loach	NH-Icon-loach	400	River	2	All day	-	-	-	-	-	-	-	-	✓	✓	✓	-
+Catfish	NH-Icon-catfish	800	Pond	4	4pm - 9am	✓	✓	✓	✓	-	-	-	-	-	-	✓	✓
+Giant snakehead	NH-Icon-giantsnakehead	5500	Pond	4	9am - 4pm	✓	✓	-	-	-	-	-	-	-	-	-	✓
+Bluegill	NH-Icon-bluegill	180	River	2	9am - 4pm	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓
+Yellow perch	NH-Icon-yellowperch	300	River	3	All day	-	-	-	✓	✓	✓	✓	✓	✓	-	-	-
+Black bass	NH-Icon-blackbass	400	River	4	All day	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓
+Tilapia	NH-Icon-tilapia	800	River	3	All day	✓	✓	✓	✓	-	-	-	-	-	-	-	✓
+Pike	NH-Icon-pike	1800	River	5	All day	-	-	✓	✓	✓	✓	-	-	-	-	-	-
+Pond smelt	NH-Icon-pondsmelt	500	River	2	All day	-	-	-	-	-	✓	✓	✓	-	-	-	-
+Sweetfish	NH-Icon-sweetfish	900	River	3	All day	✓	✓	✓	-	-	-	-	-	-	-	-	-
+Cherry salmon	NH-Icon-cherrysalmon	1000	River (Clifftop)	3	4pm - 9am	-	-	✓	✓	✓	-	-	-	✓	✓	✓	✓
+Char	NH-Icon-char	3800	River (Clifftop) Pond	3	4pm - 9am	-	-	✓	✓	✓	-	-	-	✓	✓	✓	✓
+Golden trout	NH-Icon-goldentrout	15000	River (Clifftop)	3	4pm - 9am	-	-	✓	✓	✓	-	-	-	✓	✓	✓	-
+Stringfish	NH-Icon-stringfish	15000	River (Clifftop)	5	4pm - 9am	-	-	-	-	-	✓	✓	✓	✓	-	-	-
+Salmon	NH-Icon-salmon	700	River (Mouth)	4	All day	-	-	✓	-	-	-	-	-	-	-	-	-
+King salmon	NH-Icon-kingsalmon	1800	River (Mouth)	6	All day	-	-	✓	-	-	-	-	-	-	-	-	-
+Mitten crab	NH-Icon-mittencrab	2000	River	2	4pm - 9am	-	-	✓	✓	✓	-	-	-	-	-	-	-
+Guppy	NH-Icon-guppy	1300	River	1	9am - 4pm	✓	✓	✓	✓	✓	-	-	-	-	✓	✓	✓
+Nibble fish	NH-Icon-nibblefish	1500	River	1	9am - 4pm	✓	✓	✓	-	-	-	-	-	-	-	✓	✓
+Angelfish	NH-Icon-angelfish	3000	River	2	4pm - 9am	✓	✓	✓	✓	-	-	-	-	-	-	✓	✓
+Betta	NH-Icon-betta	2500	River	2	9am - 4pm	✓	✓	✓	✓	-	-	-	-	-	-	✓	✓
+Neon tetra	NH-Icon-neontetra	500	River	1	9am - 4pm	✓	✓	✓	✓	✓	-	-	-	-	✓	✓	✓
+Rainbowfish	NH-Icon-rainbowfish	800	River	1	9am - 4pm	✓	✓	✓	✓	-	-	-	-	-	-	✓	✓
+Piranha	NH-Icon-piranha	2500	River	2	9am - 4pm & 9pm - 4am	✓	✓	✓	-	-	-	-	-	-	-	-	✓
+Arowana	NH-Icon-arowana	10000	River	4	4pm - 9am	✓	✓	✓	-	-	-	-	-	-	-	-	✓
+Dorado	NH-Icon-dorado	15000	River	5	4am - 9pm	✓	✓	✓	-	-	-	-	-	-	-	-	✓
+Gar	NH-Icon-gar	6000	Pond	5	4pm - 9am	✓	✓	✓	-	-	-	-	-	-	-	-	✓
+Arapaima	NH-Icon-arapaima	10000	River	6	4pm - 9am	✓	✓	✓	-	-	-	-	-	-	-	-	✓
+Saddled bichir	NH-Icon-saddledbichir	4000	River	4	9pm - 4am	✓	✓	✓	-	-	-	-	-	-	-	-	✓
+Sturgeon	NH-Icon-sturgeon	10000	River (Mouth)	6	All day	-	-	✓	✓	✓	✓	✓	✓	✓	-	-	-
+Sea butterfly	NH-Icon-seabutterfly	1000	Sea	1	All day	-	-	-	-	-	✓	✓	✓	✓	-	-	-
+Sea horse	NH-Icon-seahorse	1100	Sea	1	All day	✓	✓	✓	✓	✓	-	-	-	-	✓	✓	✓
+Clown fish	NH-Icon-clownfish	650	Sea	1	All day	✓	✓	✓	-	-	-	-	-	-	✓	✓	✓
+Surgeonfish	NH-Icon-surgeonfish	1000	Sea	2	All day	✓	✓	✓	-	-	-	-	-	-	✓	✓	✓
+Butterfly fish	NH-Icon-butterflyfish	1000	Sea	2	All day	✓	✓	✓	-	-	-	-	-	-	✓	✓	✓
+Napoleonfish	NH-Icon-napoleonfish	10000	Sea	6	4am - 9pm	✓	✓	-	-	-	-	-	-	-	-	-	-
+Zebra turkeyfish	NH-Icon-zebraturkeyfish	500	Sea	3	All day	✓	✓	✓	✓	✓	-	-	-	-	✓	✓	✓
+Blowfish	NH-Icon-blowfish	5000	Sea	3	6pm - 4am	-	-	-	-	✓	✓	✓	✓	-	-	-	-
+Puffer fish	NH-Icon-pufferfish	250	Sea	3	All day	✓	✓	✓	-	-	-	-	-	-	-	-	-
+Anchovy	NH-Icon-anchovy	200	Sea	2	4am - 9pm	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓
+Horse mackerel	NH-Icon-horsemackerel	150	Sea	2	All day	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓
+Barred knifejaw	NH-Icon-barredknifejaw	5000	Sea	3	All day	✓	✓	✓	✓	✓	-	-	-	✓	✓	✓	✓
+Sea bass	NH-Icon-seabass	400	Sea	5	All day	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓
+Red snapper	NH-Icon-redsnapper	3000	Sea	4	All day	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓
+Dab	NH-Icon-dab	300	Sea	3	All day	-	-	-	✓	✓	✓	✓	✓	✓	✓	-	-
+Olive flounder	NH-Icon-oliveflounder	800	Sea	5	All day	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓
+Squid	NH-Icon-squid	500	Sea	3	All day	✓	✓	-	-	-	✓	✓	✓	✓	✓	✓	✓
+Moray eel	NH-Icon-morayeel	2000	Sea	8	All day	-	✓	✓	✓	-	-	-	-	-	-	-	-
+Ribbon eel	NH-Icon-ribboneel	600	Sea	8	All day	✓	✓	✓	✓	-	-	-	-	-	-	-	✓
+Tuna	NH-Icon-tuna	7000	Pier	6	All day	-	-	-	-	✓	✓	✓	✓	✓	✓	-	-
+Blue marlin	NH-Icon-bluemarlin	10000	Pier	6	All day	✓	✓	✓	-	✓	✓	✓	✓	✓	✓	-	-
+Giant trevally	NH-Icon-gianttrevally	4500	Pier	5	All day	✓	✓	✓	✓	-	-	-	-	-	-	✓	✓
+Mahi-mahi	NH-Icon-mahimahi	6000	Pier	5	All day	✓	✓	✓	✓	-	-	-	-	-	-	✓	✓
+Ocean sunfish	NH-Icon-oceansunfish	4000	Sea	7	4am - 9pm	✓	✓	✓	-	-	-	-	-	-	-	-	-
+Ray	NH-Icon-ray	3000	Sea	5	4am - 9pm	-	✓	✓	✓	✓	-	-	-	-	-	-	-
+Saw shark	NH-Icon-sawshark	12000	Sea	7	4pm - 9am	✓	✓	✓	-	-	-	-	-	-	-	-	✓
+Hammerhead shark	NH-Icon-hammerheadshark	8000	Sea	7	4pm - 9am	✓	✓	✓	-	-	-	-	-	-	-	-	✓
+Great white shark	NH-Icon-greatwhiteshark	15000	Sea	7	4pm - 9am	✓	✓	✓	-	-	-	-	-	-	-	-	✓
+Whale shark	NH-Icon-whaleshark	13000	Sea	7	All day	✓	✓	✓	-	-	-	-	-	-	-	-	✓
+Suckerfish	NH-Icon-suckerfish	1500	Sea	9	All day	✓	✓	✓	-	-	-	-	-	-	-	-	✓
+Football fish	NH-Icon-footballfish	2500	Sea	4	4pm - 9am	-	-	-	-	✓	✓	✓	✓	✓	-	-	-
+Oarfish	NH-Icon-oarfish	9000	Sea	6	All day	-	-	-	-	-	✓	✓	✓	✓	✓	✓	-
+Barreleye	NH-Icon-barreleye	15000	Sea	2	9pm - 4am	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓
+Coelacanth	NH-Icon-coelacanth	15000	Sea (while raining)	6	All day	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓	✓`;
+
+const parsedData = northernHemisphereData.split(`\n`).map((rawRow: string) => {
+    const [ name, _imgLocation, price, location, shadowSize, spawnTime, jan, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec ] = rawRow.split(`\t`);
+    return {
+        name,
+        price,
+        location,
+        shadowSize,
+        spawnTime,
+        nMonths: {
+            jan: jan === "✓",
+            feb: jan === "✓",
+            mar: mar === "✓",
+            apr: apr === "✓",
+            may: may === "✓",
+            jun: jun === "✓",
+            jul: jul === "✓",
+            aug: aug === "✓",
+            sep: sep === "✓",
+            oct: oct === "✓",
+            nov: nov === "✓",
+            dec: dec === "✓",
+        },
+    };
+});
+
+southernHemisphereData.split(`\n`).forEach((rawRow: string) => {
+    const [ name, _imgLocation, price, location, shadowSize, spawnTime, jan, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec ] = rawRow.split(`\t`);
+    const fish = parsedData.find(row => row.name === name) as any;
+    fish && (fish.sMonths = {
+        jan: jan === "✓",
+        feb: jan === "✓",
+        mar: mar === "✓",
+        apr: apr === "✓",
+        may: may === "✓",
+        jun: jun === "✓",
+        jul: jul === "✓",
+        aug: aug === "✓",
+        sep: sep === "✓",
+        oct: oct === "✓",
+        nov: nov === "✓",
+        dec: dec === "✓",
+    });
+});
+
+console.log(JSON.stringify(parsedData));
