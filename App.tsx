db9ba27e2ef3b2550e67805bb4eb443a8a80a7b5
Noel Ray Barron <noelraybarron@hotmail.com>
2020-04-21 19:40:11 -0700

    Add basic scroll functionality to the app

--- App.tsx
+++ App.tsx
@@ -1,20 +1,28 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, ScrollView, StyleSheet, View } from 'react-native';
 import { FishTable } from './components/fish/FishTable';
+
+// https://reactnative.dev/docs/0.22/scrollview#scrolleventthrottle
+const SCROLL_EVENT_THROTTLE = 12;
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <FishTable/>
-    </View>
+    <SafeAreaView style={styles.container}>
+        <ScrollView bounces={false} style={styles.scrollView} scrollEventThrottle={SCROLL_EVENT_THROTTLE} >
+          <View>
+            <FishTable/>
+          </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 16,
+    paddingTop: 30,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  scrollView: {},
 });
