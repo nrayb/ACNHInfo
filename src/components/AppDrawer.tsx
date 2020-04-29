c6b9d21032ed37b39b14262b4ce6194898d822e9
Noel Ray Barron <noelraybarron@hotmail.com>
2020-04-29 12:22:04 -0700

    Create custom app drawer content with a button
    This is super ugly for now, but it functionally works, so
    lets keep it for now.
    
    This needs to be refined quite a bit

---
+++ src/components/AppDrawer.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  DrawerContentComponentProps,
+  DrawerContentScrollView,
+  DrawerItem,
+  DrawerItemList
+} from '@react-navigation/drawer';
+import { useHemisphereDispatch } from "../HemisphereContext"
+
+export const AppDrawer = (props: DrawerContentComponentProps) => {
+  const dispatch = useHemisphereDispatch();
+  return (
+    <DrawerContentScrollView {...props}>
+      <DrawerItemList {...props} />
+      <DrawerItem
+        label="Toggle Hemisphere"
+        onPress={() => dispatch({ type: "toggle"})}
+      />
+    </DrawerContentScrollView>
+  );
+};
