1fa1aa1f7f76518f3b117e64a211a4cd719185d1
Noel Ray Barron <noelraybarron@hotmail.com>
2020-04-29 12:21:07 -0700

    Create context to keep track of hemisphere value
    This took me a while. using context by itself wasn't hard,
    but since this was my first real time doing anything real
    with context, I wanted to learn it properly and find out
    the best way to structure my code.
    
    Who else to follow other than Kent C Dodds? Looks clean

---
+++ src/HemisphereContext.tsx
@@ -0,0 +1,51 @@
+// This is a pattern highlighted by Kent C. Dodds
+// https://kentcdodds.com/blog/how-to-use-react-context-effectively
+// I'm pretty much just copying the pattern he highlighted to learn more about contexts
+
+import React, { createContext, useReducer } from 'react';
+
+type Action = {type: 'toggle'};
+type State = "north" | "south";
+type Dispatch = (action: Action) => void;
+type HemisphereProviderProps = {children: React.ReactNode};
+
+const HemisphereStateContext = createContext<State | undefined>(undefined);
+const HemisphereDispatchContext = createContext<Dispatch | undefined>(undefined);
+
+function hemisphereReducer(state: State, action: Action) {
+  switch (action.type) {
+    case 'toggle': {
+      return state === "north" ? "south" : "north";
+    }
+    default: {
+      throw new Error(`Unhandled action type: ${action.type}`);
+    }
+  }
+}
+
+export const HemisphereProvider = ({ children }: HemisphereProviderProps) => {
+  const [state, dispatch] = useReducer(hemisphereReducer, "north");
+  return (
+    <HemisphereStateContext.Provider value={state}>
+      <HemisphereDispatchContext.Provider value={dispatch}>
+        {children}
+      </HemisphereDispatchContext.Provider>
+    </HemisphereStateContext.Provider>
+  );
+};
+
+export const useHemisphereState = () => {
+  const hemisphere = React.useContext(HemisphereStateContext)
+  if (hemisphere === undefined) {
+    throw new Error('useCountState must be used within a CountProvider')
+  }
+  return hemisphere
+};
+
+export const useHemisphereDispatch = () => {
+  const hemisphereDispatch = React.useContext(HemisphereDispatchContext)
+  if (hemisphereDispatch === undefined) {
+    throw new Error('useCountDispatch must be used within a CountProvider')
+  }
+  return hemisphereDispatch
+};
