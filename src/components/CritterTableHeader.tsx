bfc113f8442b62c7ba65effa1c1c903c1b54085e
Noel Ray Barron <noelraybarron@hotmail.com>
2020-05-04 21:15:15 -0700

    Add icon to active header

--- src/components/CritterTableHeader.tsx
+++ src/components/CritterTableHeader.tsx
@@ -1,6 +1,7 @@
 import React, { memo } from 'react';
 import { TouchableOpacity, View, Text, StyleSheet } from 'react-native';
 import { Row } from 'react-native-table-component';
+import { Icon } from 'react-native-elements';
 
 interface TableHeaderProps {
   data: string[];
@@ -9,16 +10,18 @@ interface TableHeaderProps {
   cellPressedHandler: (index: number) => void;
 }
 
+const SORT_INDICATOR_ICON_SIZE = 15;
+
 export const CritterTableHeader = memo((props: TableHeaderProps) => {
   const createHeaderCell = (headerCellData: string, index: number): React.ReactNode => {
     const { activeIndex, ascending, cellPressedHandler } = props;
-    const viewStyle = index === activeIndex ? { ...styles.headerButton, ...(ascending ? styles.activeAscendingButton : styles.activeDescendingbutton) } : styles.headerButton;
     return (
       <TouchableOpacity onPress={() => cellPressedHandler(index)}>
-        <View style={ viewStyle }>
+        <View style={ styles.headerButton }>
           <Text style={ styles.cellText }>
             { headerCellData }
           </Text>
+          { activeIndex === index && <Icon size={SORT_INDICATOR_ICON_SIZE} type="font-awesome" name={ ascending ? "sort-up" : "sort-down" } /> }
         </View>
       </TouchableOpacity>
     );
@@ -31,6 +34,4 @@ const styles = StyleSheet.create({
   container: { flexDirection: 'row', height: 50, backgroundColor: '#FFF1C1', alignContent: 'center', alignItems: "center", },
   cellText: { margin: 10, textAlign: 'center' },
   headerButton: { flexDirection: 'row', alignItems: "center", },
-  activeAscendingButton: { backgroundColor: "blue" },
-  activeDescendingbutton: { backgroundColor: "green" },
 });
